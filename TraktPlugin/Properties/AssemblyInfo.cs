d28b20d0055a5267329bc712fabd073e6305a00e
Luke Barnett <l@en.vc>
2011-03-29 10:56:46 +1300

    Updated assembly version
    Signed-off-by: Luke Barnett <l@en.vc>

--- TraktPlugin/Properties/AssemblyInfo.cs
+++ TraktPlugin/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.3.0")]
+[assembly: AssemblyFileVersion("0.9.3.0")]
 
 // MediaPortal Version Compatibility
 //[assembly: CompatibleVersion("1.1.6.27644")]
