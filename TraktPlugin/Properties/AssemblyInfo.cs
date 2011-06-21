b328fdcf9c0e0252e27ae489c47a40490fe1df19
damien <damien.haynes@cognethos.com>
2011-06-21 13:02:38 +1000

    Updated versioning to v1.0.0. Changed MP 1.2 binary to have larger revision number (should be transparent to the user), this will aid in upgrades from earlier version of MediaPortal.

--- TraktPlugin/Properties/AssemblyInfo.cs
+++ TraktPlugin/Properties/AssemblyInfo.cs
@@ -35,8 +35,14 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.0")]
-[assembly: AssemblyFileVersion("0.9.6.0")]
+// Increment revision for MediaPortal 1.2 plugin so we dont have any issues with upgrading from MP1.1.
+#if !MP12
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+#else
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+#endif
 
 // MediaPortal Version Compatibility
 #if MP12
