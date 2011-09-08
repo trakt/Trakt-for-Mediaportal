f6bfead225f1448b5f0b401fb6fef3da81597c28
damien <damien.haynes@cognethos.com>
2011-09-08 12:14:51 +1000

    Added easier API for Adding movie to watchlist which also includes optional update of MovingPictures categories/filters. You also don't need to add to a background thread as that is handled from the API.

--- TraktPlugin/TraktHelper.cs
+++ TraktPlugin/TraktHelper.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using MediaPortal.Configuration;
 using MediaPortal.Profile;
+using TraktPlugin.TraktHandlers;
+using TraktPlugin.TraktAPI;
+using TraktPlugin.TraktAPI.DataStructures;
 
-namespace TraktPlugin.GUI
+namespace TraktPlugin
 {
     public class TraktHelper
     {
@@ -58,6 +62,57 @@ namespace TraktPlugin.GUI
                 return File.Exists(Path.Combine(Config.GetSubFolder(Config.Dir.Plugins, "Windows"), "Anime2.dll")) && IsPluginEnabled("My Anime");
             }
         }
+        #endregion
+
+        #region API Helpers
+
+        public static void AddMovieToWatchList(string title, string year)
+        {
+            AddMovieToWatchList(title, year, null);
+        }
+
+        public static void AddMovieToWatchList(string title, string year, string imdbid)
+        {
+            AddMovieToWatchList(title, year, imdbid, false);
+        }
+
+        public static void AddMovieToWatchList(string title, string year, bool updateMovingPicturesFilters)
+        {
+            AddMovieToWatchList(title, year, null, updateMovingPicturesFilters);
+        }
+
+        /// <summary>
+        /// Adds a movie to the current users Watch List
+        /// </summary>
+        /// <param name="title">title of movie</param>
+        /// <param name="year">year of movie</param>
+        /// <param name="imdbid">imdbid of movie</param>
+        /// <param name="updateMovingPicturesFilters">set to true if movingpictures categories/filters should also be updated</param>
+        public static void AddMovieToWatchList(string title, string year, string imdbid, bool updateMovingPicturesFilters)
+        {
+            if (TraktSettings.AccountStatus != ConnectionState.Connected) return;
+
+            TraktMovieSync syncObject = BasicHandler.CreateMovieSyncData(title, year, imdbid);
+            if (syncObject == null) return;
+
+            Thread syncThread = new Thread(delegate(object obj)
+            {
+                TraktMovieSyncResponse response = TraktAPI.TraktAPI.SyncMovieLibrary(obj as TraktMovieSync, TraktSyncModes.watchlist);
+                if (response == null || response.Status != "success") return;
+                if (updateMovingPicturesFilters && IsMovingPicturesAvailableAndEnabled)
+                {
+                    // Update Categories & Filters
+                    MovingPictures.UpdateCategoriesAndFilters();
+                }
+            })
+            {
+                IsBackground = true,
+                Name = "Adding Movie to Watch List"
+            };
+
+            syncThread.Start(syncObject);
+        }
+
         #endregion
     }
 }
