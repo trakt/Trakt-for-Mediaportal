487d0519ae594ed6294de3a67a6e5b8bbe641f05
damien <damien.haynes@cognethos.com>
2011-09-22 16:15:35 +1000

    Incremented version to v1.1.4.

--- TraktPlugin/Properties/AssemblyInfo.cs
+++ TraktPlugin/Properties/AssemblyInfo.cs
@@ -37,11 +37,11 @@ using MediaPortal.Common.Utils;
 // [assembly: AssemblyVersion("1.0.*")]
 // Increment revision for MediaPortal 1.2 plugin so we dont have any issues with upgrading from MP1.1.
 #if !MP12
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
 #else
-[assembly: AssemblyVersion("1.1.3.1")]
-[assembly: AssemblyFileVersion("1.1.3.1")]
+[assembly: AssemblyVersion("1.1.4.1")]
+[assembly: AssemblyFileVersion("1.1.4.1")]
 #endif
 
 // MediaPortal Version Compatibility
