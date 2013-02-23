e6a2c3340bda51ca2b3cb78e0abe67d02b77b214
Damien Haynes <damien.haynes@gmail.com>
2013-02-24 09:25:44 +1100

    Published v2.6.0 release.

--- TraktPlugin/Properties/AssemblyInfo.cs
+++ TraktPlugin/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.1.0")]
-[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
 
 #region MediaPortal Compatibility
 [assembly: CompatibleVersion("1.1.6.27644")]
