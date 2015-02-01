dbe76d230872a60d28e5ba8e6030a85d58ebe429
Damien Haynes <damien.haynes@gmail.com>
2015-02-01 18:18:33 +1100

    Updated versioning to v5.1.1.

--- TraktPlugin/Properties/AssemblyInfo.cs
+++ TraktPlugin/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using MediaPortal.Common.Utils;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.1.1.0")]
+[assembly: AssemblyFileVersion("5.1.1.0")]
 
 #region MediaPortal Compatibility
 [assembly: CompatibleVersion("1.6.100.0")]
