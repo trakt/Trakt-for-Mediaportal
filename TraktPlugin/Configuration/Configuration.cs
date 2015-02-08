ef02710221b8cf7fd1900af9f6d9350cfda2add0
Damien Haynes <damien.haynes@gmail.com>
2015-02-08 18:27:41 +1100

    Ensure 'Last Paused Item' is saved when syncing playback from the configuration tool.

--- TraktPlugin/Configuration/Configuration.cs
+++ TraktPlugin/Configuration/Configuration.cs
@@ -441,6 +441,12 @@ namespace TraktPlugin
                         }
                     }
 
+                    // save last paused item so we only process after this date in future
+                    if (TraktCache.PlaybackData != null && TraktCache.PlaybackData.Count() > 0)
+                    {
+                        TraktSettings.LastPausedItemProcessed = TraktCache.PlaybackData.First().PausedAt;
+                    }
+
                     TraktLogger.Info("Library and Playback Sync completed for all enabled plugins");
                     SetSyncControlProperties(false);
                 })
